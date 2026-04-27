e5304c9c061810a1aa1c084c797f5112f09426d1
BhavanaV08 <vbhavana963@gmail.com>
2026-04-27 12:14:20 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,12 +1,26 @@
-import cv2 import pytesseract import RPi.GPIO as GPIO import time from gtts import gTTS import os
+import cv2 
+import pytesseract 
+import RPi.GPIO as GPIO 
+import time 
+from gtts import gTTS 
+import os
 
-BUTTON_PIN = 17 START_AUDIO = "/home/smartscanner/Desktop/instruction.mp3" OUTPUT_AUDIO = "/home/smartscanner/Desktop/output.mp3" IMAGE_PATH = "/home/smartscanner/Desktop/captured.jpg"
+BUTTON_PIN = 17 
+START_AUDIO = "/home/smartscanner/Desktop/instruction.mp3" 
+OUTPUT_AUDIO = "/home/smartscanner/Desktop/output.mp3" 
+IMAGE_PATH = "/home/smartscanner/Desktop/captured.jpg"
 
-def play_audio(text, audio_file): tts = gTTS(text=text, lang="en") tts.save(audio_file) os.system("amixer set Master 90%") os.system(f"mpg321 -q {audio_file}")
+def play_audio(text, audio_file): 
+    tts = gTTS(text=text, lang="en") tts.save(audio_file) os.system("amixer set Master 90%") os.system(f"mpg321 -q {audio_file}")
 
-def capture_image(image_path): os.system(f"libcamera-still -o {image_path} --timeout 10000")
+def capture_image(image_path): 
+    os.system(f"libcamera-still -o {image_path} --timeout 10000")
 
-def extract_text(image_path): img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE) img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1] text = pytesseract.image_to_string(img, lang="eng") return text.strip()
+def extract_text(image_path): 
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE) 
+    img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1] 
+    text = pytesseract.image_to_string(img, lang="eng") 
+    return text.strip()
 
 GPIO.setmode(GPIO.BCM) GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
